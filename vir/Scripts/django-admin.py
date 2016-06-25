d0044c0d8b079ff915ff825330eabb5e53ede131
William <wostensen96@gmail.com>
2016-06-25 23:10:49 +0200

    removed the folder with django lol

--- vir/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\woste\documents\python_edu\personalblog\vir\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
