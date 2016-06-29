294d903d26ee2919de97433b243ea7287c61cf42
William <wostensen96@gmail.com>
2016-06-29 15:58:15 +0200

    added my virtualenv for PythonEverywhere

---
+++ vir/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\woste\documents\python_edu\personalblog\vir\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
